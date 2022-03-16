da0a5a568011d3b2ebd7a485d0821f5602c0cc94
JohnsonLee <0xjohnsonlee@gmail.com>
2022-03-17 02:06:35 +0800

    feat: add Ok, Fail         transplant some feature of ResultType into go.

--- main.go
+++ main.go
@@ -12,14 +12,38 @@ var count = 0
 
 func (m *MyWriter) Write(a string) Result[int] {
 	if count == 2 {
-		return Result[int]{-1, errors.New("Write too many times")}
+		return Fail[int](errors.New("Write too many times"))
 	}
 	count += 1
 	fmt.Println(a)
-	return Result[int]{count, nil}
+	return Ok(len(a))
 }
 
+func (m *MyWriter) TraditionWrite(a string) (int, error) {
+	if count == 2 {
+		return -1, errors.New("Write too many times")
+	}
+	count += 1
+	fmt.Println(a)
+	return len(a), nil
+}
+func TraditionTest() {
+	count = 0
+	w := MyWriter{}
+	wrap := Wrap1(w.TraditionWrite)
+	result := wrap("Write 1").
+		Then(func() Result[int] { return wrap("Write 2") }).
+		Then(func() Result[int] { return wrap("Write 3") }).
+		Then(func() Result[int] { return wrap("Write 4") })
+
+	if result.IsFail() {
+		fmt.Println(result.ErrorOrNil())
+	} else {
+		fmt.Println(result.ValOrElse(01))
+	}
+}
 func ResultTest() {
+	count = 0
 	w := MyWriter{}
 	result := w.Write("Write 1").
 		Then(func() Result[int] { return w.Write("Write 1") }).
@@ -27,13 +51,15 @@ func ResultTest() {
 		Then(func() Result[int] { return w.Write("Write 3") }).
 		Then(func() Result[int] { return w.Write("Write 4") }).
 		Then(func() Result[int] { return w.Write("Write 5") })
-	if result.Error() != nil {
-		fmt.Println(result.Error())
+
+	if result.IsFail() {
+		fmt.Println(result.ErrorOrNil())
 	} else {
-		fmt.Println(result.Value())
+		fmt.Println(result.ValOrElse(01))
 	}
 }
 
 func main() {
+	TraditionTest()
 	ResultTest()
 }
--- result.go
+++ result.go
@@ -1,22 +1,168 @@
 package main
 
-type Result[A any] struct {
-	result A
-	err    error
+type Result[T any] interface {
+	IsOk() bool
+	IsFail() bool
+
+	ValOrElse(defaultVal T) T
+	PtrOrNil() *T
+
+	Then(func() Result[T]) Result[T]
+
+	ErrorOrNil() error
+
+	IfOk(do func(T)) Result[T]
+	OnErr(do func(error)) Result[T]
 }
 
-func (r Result[A]) Value() A {
-	return r.result
+type ok[T any] struct {
+	val T
+	Result[T]
 }
 
-func (r Result[A]) Then(f func() Result[A]) Result[A] {
-	if r.Error() == nil {
-		return f()
+func Ok[T any](val T) Result[T] {
+	result := new(ok[T])
+	result.val = val
+	return *result
+}
+func (s ok[T]) IsOk() bool {
+	return true
+}
+
+func (s ok[T]) IsFail() bool {
+	return false
+}
+
+func (s ok[T]) ValOrElse(defaultVal T) T {
+	return s.val
+}
+
+func (s ok[T]) PtrOrNil() *T {
+	return &s.val
+}
+
+func (s ok[T]) Then(f func() Result[T]) Result[T] {
+	return f()
+}
+
+func (s ok[T]) ErrorOrNil() error {
+	return nil
+}
+
+func (s ok[T]) IfOk(do func(T)) Result[T] {
+	do(s.val)
+	return s
+}
+
+func (s ok[T]) OnErr(_ func(error)) Result[T] {
+	return s
+}
+
+type fail[T any] struct {
+	err error
+	Result[T]
+}
+
+func (f fail[T]) IsOk() bool {
+	return false
+}
+func Fail[T any](err error) Result[T] {
+	fail := new(fail[T])
+	fail.err = err
+	return *fail
+}
+
+func (f fail[T]) IsFail() bool {
+	return true
+}
+
+func (f fail[T]) ValOrElse(defaultVal T) T {
+	return defaultVal
+}
+
+func (f fail[T]) PtrOrNil() *T {
+	return nil
+}
+
+func (f fail[T]) Then(do func() Result[T]) Result[T] {
+	return do()
+}
+
+func (f fail[T]) ErrorOrNil() error {
+	return f.err
+}
+
+func (f fail[T]) IfOk(_ func(T)) Result[T] {
+	return f
+}
+
+func (f fail[T]) OnErr(do func(error)) Result[T] {
+	do(f.err)
+	return f
+}
+
+func Fmap[A, B any](f func(a A) B, r Result[A]) Result[B] {
+	if r.IsOk() {
+		// not elegant
+		return Ok[B](f(r.(ok[A]).val))
 	} else {
-		return r
+		return Fail[B](r.ErrorOrNil())
 	}
 }
 
-func (r Result[A]) Error() error {
-	return r.err
+// Since method could not have type parameter, I have to write ThenDo here
+// >>=
+func ThenDo[A, B any](r Result[A], do func(A) Result[B]) Result[B] {
+	if r.IsOk() {
+		return do(r.(ok[A]).val)
+	} else {
+		return Fail[B](r.ErrorOrNil())
+	}
 }
+
+func Wrap[T any](f func() (T, error)) func() Result[T] {
+	return func() Result[T] {
+		val, err := f()
+		if err != nil {
+			return Fail[T](err)
+		} else {
+			return Ok[T](val)
+		}
+	}
+}
+
+func Wrap1[A, T any](f func(A) (T, error)) func(A) Result[T] {
+	return func(arg A) Result[T] {
+		val, err := f(arg)
+		if err != nil {
+			return Fail[T](err)
+		} else {
+			return Ok[T](val)
+		}
+	}
+}
+
+func Wrap2[A, B, T any](f func(A, B) (T, error)) func(A, B) Result[T] {
+	return func(arg1 A, arg2 B) Result[T] {
+		val, err := f(arg1, arg2)
+		if err != nil {
+			return Fail[T](err)
+		} else {
+			return Ok[T](val)
+		}
+	}
+}
+
+func Wrap3[A, B, C, T any](f func(A, B, C) (T, error)) func(A, B, C) Result[T] {
+	return func(arg1 A, arg2 B, arg3 C) Result[T] {
+		val, err := f(arg1, arg2, arg3)
+		if err != nil {
+			return Fail[T](err)
+		} else {
+			return Ok[T](val)
+		}
+	}
+}
+
+/*
+ */
